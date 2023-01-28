64bc7d24307b97c588d19c8c40083f0aacb13b00
Bhuvan S A <bhuvansa@Bhuvans-MacBook-Air.local>
2023-01-28 22:32:34 +0530

    Added Ready Queue and Updated FCFS to it

--- algos2.c
+++ algos2.c
@@ -8,86 +8,108 @@ typedef struct {
   int et;
   int tt;
   int wt;
+  int ic;
 } process;
 
+typedef struct {
+  int id;
+  int ic;
+} readyQueue;
+
+// Pro is a pointer to the memory being allocated at input
 process *pro;
-int noP, totaltime, totaltt, totalwt;
+readyQueue *rq;
+
+// noProcess ,totaltime, totatturnaroundtime, totalwaittime
+int noP, totaltime, totaltt = 0, totalwt = 0;
 
 void input(void);
 void sort(void);
 void output(void);
 void freer(void);
 void fcfs(void);
-void sjf(void);
+void npsjf(void);
 
 int main(void) {
   input();
-  sort();
+  // sort();
   fcfs();
   output();
+  // npsjf();
+  // output();
   freer();
 }
 
-// void sjf(void) {
+void npsjf(void) {
+  // Checking if there is a process in JobPool
+  totaltime = 0;
+  while (1) {
+    // Choosing a job from job pool if the at <= totaltime and ic == 0
+    for (int i = 0; i < noP; i++) {
+      if (pro[i].at <= totaltime && pro[i].ic == 0) {
+        rq[i].id = pro[i].id;
+      }
+    }
+  }
+}
+
+// Calculating times for fcfs algorithm
+// void fcfs(void) {
 //   totaltime = 0;
-//   // Loop from here
-//   while (totaltime <) {
-//     int shortt = 999999;
-//     int shorti = -1;
-//     for (int i = 0; i < noP; i++) {
-//       if (totaltime >= pro[i].at) {
-//         if (pro[i].bt < shortt) {
-//           shortt = pro[i].bt;
-//           shorti = i;
-//         }
+//   for (int i = 0; i < noP; i++) {
+//     int copybt = pro[i].bt;
+//     while (copybt > 0) {
+//       if (pro[i].at <= totaltime) {
+//         copybt--;
 //       }
-//     }
-//     if (shorti != -1) {
-//       totaltime += shortt;
-//       pro[shorti].et = totaltime;
-//       pro[shorti].tt = pro[shorti].et - pro[shorti].at;
-//       pro[shorti].wt = pro[shorti].tt - pro[shorti].bt;
-//       totaltt += pro[shorti].tt;
-//       totalwt += pro[shorti].wt;
-//     } else {
 //       totaltime++;
 //     }
+//     pro[i].et = totaltime;
+//     pro[i].tt = pro[i].et - pro[i].at;
+//     pro[i].wt = pro[i].tt - pro[i].bt;
+//     totaltt += pro[i].tt;
+//     totalwt += pro[i].wt;
 //   }
 // }
-//
-// Calculaing times for fcfs algorithm
 void fcfs(void) {
+  int j = 0;
+  int k = 0;
   totaltime = 0;
-  for (int i = 0; i < noP; i++) {
-    int copybt = pro[i].bt;
-    while (copybt > 0) {
-      if (pro[i].at <= totaltime) {
-        copybt--;
+  int t = 500;
+  while (t--) {
+    // searching for process which have arrived and adding it into the ready
+    // queue
+    j = 0;
+    for (int i = 0; i < noP; i++) {
+      if (pro[i].at <= totaltime && pro[i].ic == 0) {
+        rq[j].id = pro[i].id;
+        j++;
+        pro[i].ic++;
       }
-      totaltime++;
     }
-    pro[i].et = totaltime;
-    pro[i].tt = pro[i].et - pro[i].at;
-    pro[i].wt = pro[i].tt - pro[i].bt;
-    totaltt += pro[i].tt;
-    totalwt += pro[i].wt;
+    // If there is no process in the readyQueue then increase the currenttime
+    if (j == 0) {
+      totaltime++;
+      continue;
+    }
+    // Else if there were projects in the readyQueue then execute them
+    for (int i = 0; i < j; i++) {
+      int var = rq[i].id;
+      totaltime += pro[var].bt;
+      pro[var].et = totaltime;
+      pro[var].tt = pro[var].et - pro[var].at;
+      pro[var].wt = pro[var].tt - pro[var].bt;
+      totaltt += pro[var].tt;
+      totalwt += pro[var].wt;
+      pro[var].ic++;
+    }
   }
 }
 
 // Frees all the malloced memory spaces
-void freer(void) { free(pro); }
-
-// Sorting list according to arrival time using bubble sort
-void sort(void) {
-  for (int i = 0; i < noP - 1; i++) {
-    for (int j = 0; j < noP - 1 - i; j++) {
-      if (pro[j].at > pro[j + 1].at) {
-        process temp = pro[j];
-        pro[j] = pro[j + 1];
-        pro[j + 1] = temp;
-      }
-    }
-  }
+void freer(void) {
+  free(pro);
+  free(rq);
 }
 
 // Output the results
@@ -108,20 +130,26 @@ void output(void) {
   printf("------------------------------------------------------------------"
          "--------"
          "------------\n");
-  printf("\nTotal Execution Time : %i\n", totaltime);
-  printf("Average Turnaround Time : %i\n", totaltt / noP);
-  printf("Average Wait Time : %i\n", totalwt / noP);
+  printf("\nTotal Time Taken : %i\n", totaltime);
+  printf("Average Turnaround Time : %.2f\n", (float)totaltt / noP);
+  printf("Average Wait Time : %.2f\n", (float)totalwt / noP);
 }
 
 // Input the processes from user
 void input(void) {
-  printf("Enter no of processes");
+  printf("Enter no of processes : ");
   scanf("%d", &noP);
   pro = malloc(noP * sizeof(process));
-  printf("Process ID | Arrival Time | Burst Time\n");
+  rq = malloc(noP * sizeof(readyQueue));
+  printf("Arrival Time | Burst Time\n");
   for (int i = 0; i < noP; i++) {
     printf("----------------------------------------\n");
-    scanf("%i %i %i", &pro[i].id, &pro[i].at, &pro[i].bt);
+    scanf("%i %i", &pro[i].at, &pro[i].bt);
+    // Assigning id as the location of it as entered
+    pro[i].id = i;
+    pro[i].ic = 0;
   }
   printf("\n\n");
 }
+
+void readyQueueInit() {}
